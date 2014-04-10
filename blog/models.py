e838470e8ec54fa422b03da08d003c28a51b860b
Daily Bruin Intern <dailybruin@dailybruin.(none)>
2014-04-10 02:34:18 -0700

    first attempt at models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	"""represents a blog post"""
+	title = models.CharField(max_length=100)
+	text = models.TextField()
+	pub_date = models.DateTimeField('date published')
+
+class Comment(models.Model):
+	"""represents a comment on a blog post"""
+	post = models.ForeignKey('Post')
+	text = models.CharField(max_length=140)
+	pub_date = models.DateTimeField('date published')
