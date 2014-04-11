744e8bd0a618845dfbe505482a1f3b0de204f8fc
Daily Bruin Intern <nathancanning20@gmail.com>
2014-04-10 23:41:14 -0700

    added users and login/register urls

--- blogr/urls.py
+++ blogr/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^$', views.login, name='login'),
+	url(r'^newuser/$', views.new_user, name='new_user'),
  	url(r'^blog/', include('blog.urls')),
  	url(r'^admin/', include(admin.site.urls)),
 )
